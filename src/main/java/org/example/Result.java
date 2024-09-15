96e29023f2a00abc5d08edc36d105cca7922568f
vaskozlov <vaskozlov3940@gmail.com>
2024-09-15 12:48:13 +0300

    feat: result class

---
+++ src/main/java/org/example/Result.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Result<V, E> {
+    private final V value;
+    private final E error;
+
+    private Result(V value, E error) {
+        this.value = value;
+        this.error = error;
+    }
+
+    public static <V, E> Result<V, E> ok(V value) {
+        return new Result<>(value, null);
+    }
+
+    public static <V, E> Result<V, E> error(E error) {
+        return new Result<>(null, error);
+    }
+
+    public boolean isOk() {
+        return value != null;
+    }
+
+    public boolean isError() {
+        return !isOk();
+    }
+
+    public V getValue() {
+        if (isError()) {
+            throw new IllegalStateException("No value present");
+        }
+
+        return value;
+    }
+
+    public E getError() {
+        if (isOk()) {
+            throw new IllegalStateException("No error present");
+        }
+
+        return error;
+    }
+}
