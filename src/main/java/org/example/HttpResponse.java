90fc9128db6cf0cf79ccacfea2acad2ef9d603ae
vaskozlov <vaskozlov3940@gmail.com>
2024-09-22 14:56:25 +0300

    fix: spaces

--- src/main/java/org/example/HttpResponse.java
+++ src/main/java/org/example/HttpResponse.java
@@ -19,6 +19,7 @@ public record HttpResponse(
                 
                 %s
                 
+                
                 """.formatted(
                 version.toString(),
                 statusCode,
